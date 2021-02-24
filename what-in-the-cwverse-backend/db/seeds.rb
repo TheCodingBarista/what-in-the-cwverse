da9e018ec0e6018284131bf38ed08173ebbeac76
melissa_holt <melissa.holt21@gmail.com>
2021-02-23 22:16:50 -0800

    Change style, reset db

--- what-in-the-cwverse-backend/db/seeds.rb
+++ what-in-the-cwverse-backend/db/seeds.rb
@@ -1,5 +1,14 @@
-Character.create([
-    {name: 'Felicity Smoak', show: shows.second, known_for: ["intelligence", "technology"]},
-    {name: 'John Diggle', show: shows.second, known_for: "combat"},
-])
+#Character.create([
+ #   {name: 'Felicity Smoak', show: shows.second, known_for: ["intelligence", "technology"]},
+ #   {name: 'John Diggle', show: shows.second, known_for: "combat"},
+#])
 
+@show_ids = ["3", "430", "605"]
+
+    @show_ids.each do |id|
+        response = JSON.parse(RestClient.get("https://www.comicvine.com/api/series_list/?api_key=3f76c9a84c976ae891f3ae40cbb2526dd6fac07b&filter=id:#{id}&field_list=name,deck,description,image,last_episode,api_detail_url&format=json").body)
+        show = Show.create!(
+            :title => response["results"][0]["name"],
+            :description => response["results"][0]["deck"] || response["results"][0]["description"]
+        )
+    end
--- what-in-the-cwverse-frontend/src/index.js
+++ what-in-the-cwverse-frontend/src/index.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  alert('This is a test');
+  //alert('This is a test');
   console.log('Test');
 });
-
-import gideonapi from 'gideon-api';
-
-//fetch a random Arrowverse quote:
-const quote = await gideonapi.quote();
-console.log(quote);
