f8fd05ba5fddfc949c770d7f37c79f1c448257a0
melissa_holt <melissa.holt21@gmail.com>
2021-03-02 17:14:42 -0800

    Change order of js files so that function call swork properly
    Simplify index.html file

--- what-in-the-cwverse-frontend/src/index.js
+++ what-in-the-cwverse-frontend/src/index.js
@@ -1,21 +1,6 @@
 console.log("Yoooo, we in here");
 
-document.addEventListener('DOMContentLoaded', () => {
-  //alert('This is a test');
+document.addEventListener("DOMContentLoaded", () => {
   getShows();
   console.log('Test');
 })
-
-function getShows() {
-  fetch("http://localhost:3000/shows")
-  .then(resp => resp.json())
-  .then(data => renderShows(data))
-}
-
-function renderShows(data) {
-  let showsIndex = document.getElementById("all-shows")
-  data.forEach((show) => {
-    let newShow = new Show(show)
-  })
-}
-
--- what-in-the-cwverse-frontend/src/show.js
+++ what-in-the-cwverse-frontend/src/show.js
@@ -5,3 +5,25 @@ class Show {
         this.description = data.description;
     }
 }
+
+function getShows() {
+    fetch("http://localhost:3000/shows")
+    .then(resp => resp.json())
+    .then(data => renderShows(data))
+    //.then(data => console.log(data))
+  }
+  
+  Show.prototype.displayShow = function() {
+    return `<div class="show-card">
+            <h4 class="show-title">${this.title}</h4><br></br>
+            <p>${this.description}</p><br></br>
+          </div>`
+  }
+  
+  function renderShows(data) {
+    let showsIndex = document.getElementById("all-shows")
+    data.forEach((show) => {
+      let newShow = new Show(show)
+      showsIndex.innerHTML += newShow.displayShow()
+    });
+  }
